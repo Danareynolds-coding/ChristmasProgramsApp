ce7c2321cd70be7f8d1f6eee917cc1cf4ed5779c
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-11-22 21:55:07 -0600

    set up server

--- server.js
+++ server.js
@@ -2,4 +2,18 @@ const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 3000
 
+const cors = require('cors')
+const helmet = require('helmet')
+server.use(cors())
+server.use(helmet.contentSecurityPolicy({
+  useDefaults: true,
+  crossOriginsResourcePolicy:false,
+  crossOriginsEmbedderPolicy:false,
+  diectives: {
+    "img-src":["'self'", "https: data"],
+    "scriptSrc":["'self'", "cdn.jsdelivr.net"]
+  }
+}))
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
 server.listen(PORT, ()=> console.log(`Merry Christmas!!! at port {PORT}`))
